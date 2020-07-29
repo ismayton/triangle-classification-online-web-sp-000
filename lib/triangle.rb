a34931c4d741a250f05590ab2c0393103df4ac1a
Ian Mayton <ian.mayton@gmail.com>
2020-07-29 15:15:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,19 @@
 class Triangle
   
-  attr_accessor 
-  def initialize(attributes)
-   attributes.each {|key, value| self.send("#{key}=", value)
+  attr_accessor :one, :two, :three 
+  
+  def initialize(one, two, three)
+    @one = one 
+    @two = two 
+    @three = three
  end 
  
+ def self.valid?
+   if @one == 0 || @two == 0 || @three == 0
+     return false 
+     elsif 
+     
+ def kind 
+   
+     
 end
